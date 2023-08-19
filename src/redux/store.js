bd5a7ba0b997bdddcaf33affa311b7bd84059cce
Vitalii-Dashivets <122096100+Vitalii-Dashivets@users.noreply.github.com>
2023-08-19 14:57:59 +0300

    Edit store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { filterReducer } from './filterSlice';
 import { persistedContactsReducer } from './contactsSlice';
 
@@ -7,6 +15,13 @@ export const store = configureStore({
   reducer: {
     contacts: persistedContactsReducer,
     filter: filterReducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
   },
 });
 
